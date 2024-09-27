a764ea1559c823f3e155d7236fe4249a2635079e
Patrick Baus <patrick.baus@physik.tu-darmstadt.de>
2024-09-27 13:03:16 +0200

    Set the UART timeout for blocking writes to 1s

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -645,7 +645,7 @@ PUTCHAR_PROTOTYPE
 {
   /* Place your implementation of fputc here */
   /* e.g. write a character to the USART1 and Loop until the end of transmission */
-  HAL_UART_Transmit(&huart4, (uint8_t *)&ch, 1, 0xFFFF);
+  HAL_UART_Transmit(&huart4, (uint8_t *)&ch, 1, 1000);
 
   return ch;
 }
